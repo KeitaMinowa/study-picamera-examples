0e4d0228fd033dd879b6f22637d908396d2bdcfb
KeitaMinowa <38806762+KeitaMinowa@users.noreply.github.com>
2018-05-29 20:43:18 +0900

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -12,10 +12,11 @@ class FaceDetector(object):
         self.vs = PiVideoStream(resolution=(800, 608)).start()
         self.flip = flip
         time.sleep(2.0)
+        
         self.argvs = sys.argv
--       self.face_cascade = cv2.CascadeClassifier('camera/processor/model/haarcascades/haarcascade_frontalface_default.xml')
--       if len(self.argvs) >= 2 and "eyes" in self.argvs:
--           self.eye_cascade = cv2.CascadeClassifier('camera/processor/model/haarcascades/haarcascade_eye.xml')
+        self.face_cascade = cv2.CascadeClassifier('camera/processor/model/haarcascades/haarcascade_frontalface_default.xml')
+        if len(self.argvs) >= 2 and "eyes" in self.argvs:
+            self.eye_cascade = cv2.CascadeClassifier('camera/processor/model/haarcascades/haarcascade_eye.xml')
 
         # opencvの顔分類器(CascadeClassifier)をインスタンス化する
 
@@ -35,15 +36,15 @@ class FaceDetector(object):
 
     def process_image(self, frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
--       faces = self.face_cascade.detectMultiScale(gray, 1.3, 3)
--       for (x,y,w,h) in faces:
--           cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
--           if len(self.argvs) >= 2 and "eyes" in self.argvs:
--                roi_gray = gray[y:y+h, x:x+w]
--                roi_color = frame[y:y+h, x:x+w]
--                eyes = self.eye_cascade.detectMultiScale(roi_gray)
--                for (ex,ey,ew,eh) in eyes:
--                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 3)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            if len(self.argvs) >= 2 and "eyes" in self.argvs:
+                 roi_gray = gray[y:y+h, x:x+w]
+                 roi_color = frame[y:y+h, x:x+w]
+                 eyes = self.eye_cascade.detectMultiScale(roi_gray)
+                 for (ex,ey,ew,eh) in eyes:
+                     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
         # opencvでframe(カラー画像)をグレースケールに変換
 
         return frame
